09607b4b4e2b6826e64efa789ee10fa5f0ce07ef
LopesSoh <sophiac.lopes2008@gmail.com>
2025-09-09 13:33:40 -0300

    def limpar e pausar

--- funcs.py
+++ funcs.py
@@ -5,4 +5,6 @@ def menu_principal():
     print("︿︿︿︿︿ 〔MENU LOCADORA〕 ︿︿︿︿︿")
     print(" 1- Cadastrar \n 2- Listar \n 3- Alugar/Devolver \n 0- Sair ")
 
-
+def LP():
+    os.system('pause')
+    os.system('cls')
